6097428d436ff66dd404eaa5ee79d9e640638cb3
yokoyama <t.yokoyama@kannart.co.jp>
2024-05-08 16:45:46 +0900

    any

--- samples/test.php
+++ samples/test.php
@@ -3,10 +3,31 @@
 
 class Test
 {
+    protected int|string|array|object $any;
+
     public function __construct(
         protected string $name = 'World',
-    )
+    ) {
+    }
+
+    protected function all(): array
     {
+        return [
+            'name' => $this->name,
+        ];
+    }
+
+    protected function getSingle(int|string $id): object
+    {
+        return (object) [
+            'id' => $id,
+            'name' => $this->name,
+        ];
+    }
+
+    protected function setProperty(int|string|array|object $any): void
+    {
+        $this->any = $any;
     }
 
     public function setName(string $name): void
